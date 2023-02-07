5e9ee6fe2243f3efb7a111c283ee7f7c1557f0a0
“Prescilla <21103870@usc.edu.ph>
2023-02-07 14:37:05 +0800

    add admin page

--- frontend/my-app/src/app/app.module.ts
+++ frontend/my-app/src/app/app.module.ts
@@ -3,11 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { UserComponent } from './artifial/user/user.component';
+import { DashboardComponent } from './admin/dashboard/dashboard.component';
+import { TransactionsComponent } from './admin/transactions/transactions.component';
+import { ActivitiesComponent } from './admin/activities/activities.component';
+import { SettingsComponent } from './admin/settings/settings.component';
+import { FeedbacksComponent } from './admin/feedbacks/feedbacks.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    DashboardComponent,
+    TransactionsComponent,
+    ActivitiesComponent,
+    SettingsComponent,
+    FeedbacksComponent
   ],
   imports: [
     BrowserModule
